aeb158c23d53f51d72dc96cab3d78ca4854cbc3a
nilsb <n.baierl1@oth-aw.de>
2024-05-31 20:58:35 +0200

    restructured to fit as required (hopefully)

---
+++ sys-src/frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Button />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
