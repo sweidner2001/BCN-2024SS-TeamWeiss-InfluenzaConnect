2798f84eafe6bc713801f3f9e235abba0d6e5a13
Sebastian Weidner <s.weidner@oth-aw.de>
2024-06-01 13:23:22 +0200

    Add: Windows Setup eingerichtet + anpassung der .sh-Dateien für Windows WSL

---
+++ sys-src/devenv_windows/setup_devenv.ps1
@@ -0,0 +1,41 @@
+#!/usr/bin/env pwsh
+
+# Function for error checking
+function check_result {
+    param (
+        [string]$message
+    )
+    if ($LASTEXITCODE -ne 0) {
+        Write-Error "Error: $message"
+        exit 1
+    }
+}
+
+# Change directory
+Set-Location -Path ..
+
+# Create virtual environment
+python -m venv backend
+check_result "Failed to create virtual environment"
+
+# Activate virtual environment
+$venvActivatePath = ".\backend\Scripts\Activate.ps1"
+if (Test-Path $venvActivatePath) {
+    & $venvActivatePath
+    check_result "Failed to activate virtual environment"
+} else {
+    Write-Error "Activation script not found"
+    exit 1
+}
+
+# Install requirements
+pip install -r .\backend\requirements.txt
+check_result "Failed to install requirements"
+
+# Docker-Compose Build
+docker-compose -f devenv_windows\docker-compose.yml up
+check_result "Failed to start Docker Compose"
+
+Write-Output ""
+Write-Output "----------------------------------------"
+Write-Output "Development environment setup complete."
