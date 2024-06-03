bbcfd75ef9ba3bf611903db4e92e36142b3bff9d
nilsb <n.baierl1@oth-aw.de>
2024-06-03 10:08:01 +0200

    npx tailwindcss init | und tailwind einbindung erfolgreich :)

--- sys-src/frontend/src/components/ButtonSend2DB.tsx
+++ sys-src/frontend/src/components/ButtonSend2DB.tsx
@@ -1,19 +1,24 @@
-// ein Button der mit einer Get request die Daten an die DB sendet
-
 import React from 'react';
 import axios from 'axios';
 
 const ButtonSend2DB = () => {
   const handleClick = async () => {
     try {
-        const response = await axios.get('http://localhost:5001/testdb');
-        console.log(response.data);
+      const response = await axios.get('http://localhost:5001/testdb');
+      console.log(response.data);
     } catch (error) {
       console.error('Error fetching data: ', error);
     }
   };
 
-  return <button onClick={handleClick}>testInsertInDB</button>;
+  return (
+    <button
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      onClick={handleClick}
+    >
+      Insert into DB
+    </button>
+  );
 };
 
 export default ButtonSend2DB;
