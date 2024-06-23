16f687cc9e7fdc4d421e533be766242d467b9906
TimonSpichtinger <132363851+TimonSpichtinger@users.noreply.github.com>
2024-06-23 18:09:45 +0200

    "test für frontend hinzugefügt"

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+};
+
+export default config;
